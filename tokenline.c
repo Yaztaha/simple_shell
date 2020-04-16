23a609046fd2a45f878da659ed66e3fa28dd01e2
Taha Yazidi <yazi.taha@gmail.com>
2020-04-16 22:17:13 +0000

    mem_corruption_fix

--- tokenline.c
+++ tokenline.c
@@ -21,7 +21,7 @@ char **tokenline(char *line)
 		if (line[i] == ' ')
 			token_num++;
 	}
-	tokens = malloc(sizeof(char *) * (token_num + 2));
+	tokens = malloc(sizeof(char *) * (token_num + 1));
 	if (tokens == NULL)
 	{
 		free(tokens);
